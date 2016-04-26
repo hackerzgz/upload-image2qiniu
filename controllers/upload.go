5765082cb4d1b7d2f47466300f27b76b7b178973
HackeZ <767110505@qq.com>
2016-04-26 11:13:28 +0800

    添加公网设置

--- controllers/upload.go
+++ controllers/upload.go
@@ -20,6 +20,14 @@ type PutRet struct {
 	Key  string `json:"key"`
 }
 
+// 页面返回值变量
+var ReturnValue struct {
+	Flag     bool
+	Title    string
+	Tips     string
+	FilePath string
+}
+
 type UploadController struct {
 	beego.Controller
 }
@@ -27,10 +35,13 @@ type UploadController struct {
 func (this *UploadController) Get() {
 	// ===============  Cache Test
 	this.Data["AKEY"], this.Data["SKEY"] = ReadCache()
-	fmt.Println("AK, SK -->")
 	// ===============  Cache Test
-	this.Data["Title"] = "Upload Image 2 QiNiu"
-	this.Data["Tips"] = "一旦上传成功，会将你上传成功的AK以及SK进行加密缓存10min，这时候之后只要你不重新刷新页面，你依然不需要重新CV你的AK以及SK"
+	if !ReturnValue.Flag {
+		setReturnValue("Upload Image 2 QiNiu", "一旦上传成功，会将你上传成功的AK以及SK进行加密缓存10min，这时候之后只要你不重新刷新页面，你依然不需要重新CV你的AK以及SK", "")
+	}
+	this.Data["Title"] = ReturnValue.Title
+	this.Data["Tips"] = ReturnValue.Tips
+	this.Data["Filepath"] = ReturnValue.FilePath
 	this.TplName = "upload/index.html"
 }
 
@@ -54,19 +65,24 @@ func (this *UploadController) Post() {
 	// 简易上传
 	ret, res := SimpleUploadFile(bucket, h.Filename)
 	//打印返回的Hash,Key信息
-	// fmt.Println(ret)
+	fmt.Println(ret)
 	//打印出错信息
 	if res != nil {
 		Jump2ErrPage(this, res)
 	} else {
+		fmt.Println("I am In!")
 		// 将AK以及SK缓存到Cache模块中
 		setCache(this.GetString("a-k"), this.GetString("s-k"))
-		this.Data["AKEY"], this.Data["SKEY"] = ReadCache()
-		this.Data["Title"] = "Upload Image 2 QiNiu"
-		this.Data["Tips"] = "Upload Success!"
-		this.Data["filepath"] = "FilePath: <公网地址>/" + ret.Key
-		this.TplName = "upload/index.html"
+		setReturnValue("Upload Image 2 QiNiu", "Upload Success!", GetFilePath()+ret.Key)
+		ReturnValue.Flag = true
+		// 303防止POST提交之后刷新出现重新提交操作
+		this.Redirect("/upload", 303)
 	}
+}
+
+// 设置页面返回值提示
+func setReturnValue(title, tips, filepath string) {
+	ReturnValue.Title, ReturnValue.Tips, ReturnValue.FilePath = title, tips, filepath
 }
 
 // 简易上传
@@ -103,10 +119,10 @@ func ReadCache() (ak, sk string) {
 
 // 设置AK/SK缓存
 func setCache(ak, sk string) {
-	if err := bm.Put("AK", ak, 10*time.Second); err != nil {
+	if err := bm.Put("AK", ak, 10*time.Minute); err != nil {
 		fmt.Println("AK Cache Faile!")
 	}
-	if err := bm.Put("SK", sk, 10*time.Second); err != nil {
+	if err := bm.Put("SK", sk, 10*time.Minute); err != nil {
 		fmt.Println("SK Cache Faile!")
 	}
 }
@@ -115,5 +131,15 @@ func setCache(ak, sk string) {
 func Jump2ErrPage(this *UploadController, err error) {
 	this.Data["res"] = "文件上传出错了：" + err.Error()
 	this.TplName = "error.html"
+	return
+}
+
+// 获取设置中的公网地址
+func GetFilePath() (filePath string) {
+	fmt.Println(beego.AppConfig.String("PublicPath"))
+	if filePath = beego.AppConfig.String("PublicPath"); filePath == "" {
+		filePath = "FilePath: <公网地址>"
+	}
+	filePath += "/"
 	return
 }
